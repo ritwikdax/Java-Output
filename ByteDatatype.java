f655b4470a4d6613c1635a72f7de49234ea11a68
Ritwik Das <53152719+devritwik@users.noreply.github.com>
2019-10-19 22:51:40 +0530

    Update and rename Main.java to ByteDatatype.java

--- ByteDatatype.java
+++ ByteDatatype.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class ByteDatatype {
 
     public static void main(String[] args) {
 	// write your code here
